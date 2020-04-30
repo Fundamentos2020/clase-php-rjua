d70e480a24532455457b83131886c42f8fa3efa6
rjua <gerrero12@live.com>
2020-04-30 18:16:57 -0500

    clase martes

--- index.php
+++ index.php
@@ -1,17 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<h1>prueba php</h1>
-
 <?php
-    echo"hola mundo";
 
+//declaracion de variables
+
+$a="hola mmundo";
+$b= 3;
+$c= 2.5;
+
+//echo $a," ",$b," ",$c;
+
+// areglos 
+
+$array_1 = array();
+$alumno = array();
+
+$array_1[]= 1;
+$array_1[]= 2;
+$array_1[]= 3;
+
+//var_dump($array_1);
+
+$alumno["claveunica"]= "241584";
+$alumno["nombre"]= "josue";
+$alumno["apellido"]= "sanchez";
+
+//json
+
+$json='{"claveunica":"241584","nombre":"josue","apellido":"sanchez"}';  
+$alumno_aux = json_decode($json);
+
+echo $alumno_aux->claveunica;
 ?>
-</body>
-</html>
